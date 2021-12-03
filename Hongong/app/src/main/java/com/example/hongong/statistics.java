1ae9df65871a3f1f0bcb4dde383f33777cb3d4de
ChoiSenn <quitechoisen@gmail.com>
2021-12-03 23:44:43 +0900

    page set

--- Hongong/app/src/main/java/com/example/hongong/statistics.java
+++ Hongong/app/src/main/java/com/example/hongong/statistics.java
@@ -1,12 +1,31 @@
 package com.example.hongong;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class statistics extends MainActivity {
+@SuppressWarnings("deprecation")
+public class statistics extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecDay = tabHost.newTabSpec("Day").setIndicator("일별");
+        tabSpecDay.setContent(R.id.tabDay);
+        tabHost.addTab(tabSpecDay);
+
+        TabHost.TabSpec tabSpecWeek = tabHost.newTabSpec("Week").setIndicator("주별");
+        tabSpecWeek.setContent(R.id.tabWeek);
+        tabHost.addTab(tabSpecWeek);
+
+        TabHost.TabSpec tabSpecMonth = tabHost.newTabSpec("Month").setIndicator("월별");
+        tabSpecMonth.setContent(R.id.tabMonth);
+        tabHost.addTab(tabSpecMonth);
+
+        tabHost.setCurrentTab(0);
     }
 }
